f76485fe561f6e48706e52cd7213b30994a0a8f3
Parham <54209786+ParhamVA@users.noreply.github.com>
2019-10-27 19:49:30 +0330

    Initial commit

---
+++ app/src/main/java/com/example/splashscreenapp/Splash.java
@@ -0,0 +1,44 @@
+package com.example.splashscreenapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        Thread threadTimeSplash = new Thread(){
+
+            @Override
+            public void run(){
+
+                try{
+                    sleep(3000);
+                }catch (Exception e){
+
+                }finally {
+                    startActivity(new Intent(Splash.this,MainActivity.class));
+                }
+
+            }
+
+
+        };
+
+        threadTimeSplash.start();
+
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
